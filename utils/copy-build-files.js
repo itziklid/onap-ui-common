c6e005714f3ed2e5c612a2fcbe90809851183b0b
Israel Lavi <israel.lavi@gmail.com>
2018-07-08 16:40:51 +0300

    work in progress

--- utils/copy-build-files.js
+++ utils/copy-build-files.js
@@ -7,6 +7,6 @@ copy('components/**/*.scss', 'lib/components/', function(err, file) {
   // exposes the vinyl `file` created when the file is copied
 });
 
-copy(['index.js', 'package.json'], 'lib/', function(err, file) {
-    // exposes the vinyl `file` created when the file is copied
-});
+// copy(['index.js', 'package.json'], 'lib/', function(err, file) {
+//     // exposes the vinyl `file` created when the file is copied
+// });
